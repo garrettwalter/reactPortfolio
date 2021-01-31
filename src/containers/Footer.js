f35bfe888466948cd7e1263232a51dff9e158e33
Garrett Walter <gtwalter150@gmail.com>
2021-01-31 18:50:03 -0500

    made icons on footer links to site

--- src/containers/Footer.js
+++ src/containers/Footer.js
@@ -6,20 +6,19 @@ function Footer(props) {
             <nav className="navbar-expand">
             <ul className="navbar-nav justify-content-evenly">
             <li className="nav-item">
-                        <img 
-                        id="icon" 
-                        src="https://images.vexels.com/media/users/3/155690/isolated/preview/cfa6b24903a8af399de6c515baeb14fd-black-envelope-square-icon-by-vexels.png"
-                        href="mailto:astronet@gmail.com"/>
+                        <a href="mailto:astronet@gmail.com">
+                            <img id="icon" src="https://images.vexels.com/media/users/3/155690/isolated/preview/cfa6b24903a8af399de6c515baeb14fd-black-envelope-square-icon-by-vexels.png" alt="email"/>
+                        </a>
                     </li>
             <li className="nav-item">
-                        <img id="icon" 
-                        src="https://i0.wp.com/jasonbgraham.com/wp-content/uploads/2020/04/jason-b-graham-linkedin-icon-square-0073b1-featured-image.png?fit=2000%2C2000&ssl=1"
-                        href="https://www.linkedin.com/in/garrett-walter-63b2b81b5"/>
+                        <a href="https://www.linkedin.com/in/garrett-walter-63b2b81b5">
+                            <img id="icon" src="https://i0.wp.com/jasonbgraham.com/wp-content/uploads/2020/04/jason-b-graham-linkedin-icon-square-0073b1-featured-image.png?fit=2000%2C2000&ssl=1" alt="linkedIn"/>
+                        </a>
                     </li>
             <li className="nav-item">
-                        <img id="icon" 
-                        src="https://image.flaticon.com/icons/png/512/38/38401.png"
-                        href="https://github.com/garrettwalter"/>
+                        <a href="https://github.com/garrettwalter">
+                            <img id="icon" src="https://image.flaticon.com/icons/png/512/38/38401.png" alt="gitHub"/>
+                        </a>
                     </li>
             </ul>
             </nav>
