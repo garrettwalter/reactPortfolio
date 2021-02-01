5f26d4afbef70ef6e0a5d9e0edc1740faad1975d
Garrett Walter <gtwalter150@gmail.com>
2021-02-01 00:37:20 -0500

    added repo links to project pages

--- src/App.js
+++ src/App.js
@@ -17,12 +17,12 @@ const App = () => {
           <Route exact path="/projects" component={Projects} />
           <Route exact path="/resume" component={Resume} />
           <Route exact path="/about" component={About} />
-          <Route exact path="/astronet" ><Project project="https://astronetdigital.com" title="Astronet"/></Route>
+          <Route exact path="/astronet" ><Project project="https://astronetdigital.com" title="Astronet" repo="https://github.com/garrettwalter/astronet"/></Route>
           <Route exact path="/covidcost" ><Project project="https://roccomaniscalco.github.io/covid-cost/" title="Covid-Cost" repo="https://github.com/garrettwalter/covid-cost"/></Route>
-          <Route exact path="/neonpeach" ><Project project="https://neon-peach.herokuapp.com/" title="Neon Peach"/></Route>
-          <Route exact path="/dayplanner" ><Project project="https://garrettwalter.github.io/daily-planner/" title="Day Planner"/></Route>
-          <Route exact path="/userdirectory" ><Project project="https://garrettwalter.github.io/user-directory/" title="User Directory"/></Route>
-          <Route exact path="/budgetapp" ><Project project="https://nameless-reef-14501.herokuapp.com/" title="Budget App"/></Route>
+          <Route exact path="/neonpeach" ><Project project="https://neon-peach.herokuapp.com/" title="Neon Peach" repo="https://github.com/garrettwalter/project-2-legislations"/></Route>
+          <Route exact path="/dayplanner" ><Project project="https://garrettwalter.github.io/daily-planner/" title="Day Planner" repo="https://github.com/garrettwalter/daily-planner"/></Route>
+          <Route exact path="/userdirectory" ><Project project="https://garrettwalter.github.io/user-directory/" title="User Directory" repo="https://github.com/garrettwalter/user-directory"/></Route>
+          <Route exact path="/budgetapp" ><Project project="https://nameless-reef-14501.herokuapp.com/" title="Budget App" repo="https://github.com/garrettwalter/budget-app"/></Route>
           <Footer/>
       </Router>
     );
