dfbb79a5dd993df41e99670ab8632b152498091f
Garrett Walter <gtwalter150@gmail.com>
2021-02-01 13:23:36 -0500

    about page skeleton

--- src/containers/About/About.jsx
+++ src/containers/About/About.jsx
@@ -2,8 +2,22 @@ import React from 'react';
 
 function About(props) {
     return (
-        <div>
-            <h1>About</h1>
+        <div className="container about-box">
+            <div className="row">
+                <h1 className="text-center blue">Hi, i'm Garrett</h1>
+            </div>
+            <div className="row justify-content-center">
+                <img src="./me.jpg" alt="me"/>
+            </div>
+            <div className="row">
+                <h3 className="text-center blue">Hi, i'm Garrett</h3>
+            </div>
+            <div className="row justify-content-center">
+                <img alt="me standing" src="./me2.jpg"/>
+            </div>
+            <div className="row">
+                <h3 className="text-center blue">Hi, i'm Garrett</h3>
+            </div>
         </div>
     );
 }
